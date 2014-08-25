cb7b3679829a42f0404f6775d941d04bafefe4a0
Alex Chen <axc_elnino@hotmail.com>
2014-08-25 13:00:32 -0600

    Replacing old potentials.py with a new draft by replacing the entire file.

--- src/potentials.py
+++ src/potentials.py
@@ -2,142 +2,69 @@
 
 import numpy as np
 import itertools
-import sys
+from operator import mul
 
 
-def harmonic(x,forceConst,xopt):
-    ''' Harmonic potential
-    
-    Parameters
-    ------------
-    forceConst : array
-    xopt:    array
-    x:    array
-    
-    Returns
-    ----------
-    U : array
-    '''
-    
-    U = 0.5 * forceConst * np.square(x - xopt)
-    
-    return U
-    
-
-def cosine(x,forceConst,xopt):
-    ''' Cosine potential
-    
-    Parameters
-    ------------
-    forceConst : array
-    xopt:    array
-    x:    array
-    
-    Returns
-    ----------
-    U : array
-    '''
-    
-    U = forceConst * (1.0 - np.cos(np.radians(x - xopt)))
-    
-    return U
-
-
-def biasPotential(biasType,collvars,vardict,histEdges):
-    ''' calculate biasing potential on every bin
-    
-     Parameters
-    -------------
-        biasType: list
-            potential functions to use [ "harmonic" or "cosine"]
-        collvars: list
-            keys of collvars
-        vardict: dict
-            collvars as keys and parameter list as values
-        histEdges: 
-            list of edges of histogram
-    Returns
-    ------------
-        Ub: array 
-            shape [nbin_collvar1,nbin_collvar2... nbin_ncolvarN,Nsimulations]
-    
-    Notes
-    -------
-        
-    '''
-    
-    biasFuncts=[]
-    for i in biasType:
-        
-        if i == "harmonic":
-            biasFuncts.append(harmonic)
-        elif i == "cosine":
-            biasFuncts.append(cosine)
-        else:
-            errstr = "%s Not implimented"%i
-            raise SystemExit(errstr)
-    
-    histMidPoints = [(edge[1:] + edge[:-1])*0.5 for edge in histEdges]
-    
-    varZipped = [zip(vardict[i+'_fc'],vardict[i + '_x0']) for i in collvars]
-        
-    # for every point evaluate function with these paramaters
-    fargs = list(itertools.product(*varZipped))
-    #Dimensions of Ub is dimension of histogram + 1
-    # size of each dimenstion is number of gridpoints in that dimension
-    # the last dimension is total number of simulations
-        
-    UbDim = [len(midpoints) for midpoints in histMidPoints ]
-    # Total number of simulations
-    nsim = 1
-    
-    for j in collvars:
-        nsim = nsim * len(vardict[j])
-    
-    
-    UbDim.append(nsim)
-    Ub = np.zeros(UbDim,dtype = np.float64)
-    
-    for i,j in np.ndenumerate(Ub):
-        Ub[i] = _eval_potential(i,biasFuncts,fargs,histMidPoints)
-        #print i, Ub[i]
-        #sys.exit()
-    
-    return Ub
-    
+# Expose only this function:
+def biasingPotentials(?):
+    return _combinePotentials( _readFunctions(?)
+                             , _readArgLists(?)
+                             , _readEdgeLists(?)
+                             )
             
-        
-        
-def _eval_potential(ind,biasFuncts,fargs,histMidpoints):
-    ''' evaluate potential for a given point with given functions and arguments
-    
-    parameters
-    -------------
-    ind: tuple
-        index of Ub array
-    biasFuncts:
-        list of functions
-    fargs:
-        function arguments for each function
-    histmidpoints: list of arrays
-    
-    returns
-    ---------
-    U: float
-        potential energy
-        
-    '''
-    
-    ndim = len(ind) - 1
-    fargInd = ind[-1]
-    U = 0.0
-    for i in range(ndim):
-        point = histMidpoints[i][ind[i]]
-        U = U + biasFuncts[i](point,*fargs[fargInd][i])
-        #print point,fargs[fargInd][i], biasFuncts[i](point,*fargs[fargInd][i])
-    return U
-        
-    
-    
-    
+# Transform functions, argLists, edgeLists from whatever form in which they are available.
+def _readFunctions(functionNames):
+    return [ lookupDict_potentials[fn] for fn in functionNames ]
+
+def _readArgLists(vardict, collvars):
+    return [ zip(vardict[i+'_fc'],vardict[i + '_x0']) for i in collvars ]
+
+def _readEdgeLists(?):
+    pass
+
+# I am not yet sure in what forms the arguments of this function are available.
+# At the moment, it takes a list of functions (fs[dim])
+#                         a list of lists of argument tuples (argLists[dim][i]), and
+#                         a list of lists of scalars (edgeLists[dim][i]),
+#     where indexing is shown by e.g. [dim] (reaction coordinate dimension index), or
+#                                     [i]   (unnamed index).
+# :: [(Arg -> Double -> Double)] -> [[Arg]] -> [[Double]] -> [[Double]]
+def _biasingPotentials(fs, argLists, edgeLists):
+    simulationParameters = list(itertools.product(*argLists))
+    simulationPotentials = map(lambda p: _combinePotentials(fs, p), simulationParameters)
+    binCoordinates = list(itertools.product(*map(_binCenters, edgeLists)))
+    return np.reshape((len(simulationPotentials), len(binCoordinates))
+                      np.array([ f(p) for f in simulationPotentials for p in binCoordinates ])
+                      )
+
+# :: [(Arg -> Double -> Double)] -> [Arg] -> (Vector Double -> Double)
+def _combinePotentials(fs, argTuples):
+    def combinedPotential(xs):
+        acc = 0.0
+        for i in xrange(len(xs)):
+            acc += fs[i](xs[i], *argTuples[i])
+        return acc
+    return combinedPotentials
+
+# :: [Double] -> [Double]
+def _binCenters(edgeList):
+    return  0.5*(edgeList[1:] + edgeList[:-1])
+
+
+# ----------------------------------------------------------------
+# Potential functions.
+# Note: potential functions must have call signatures of the type (Double, *args).
+
+lookupDict_potentials = { "harmonic"  : potential_harmonic
+                        , "cosine"    : potential_cosine
+                        }
+
+def potential_harmonic(x, x0, k):
+    """Evaluate at 'x' a harmonic potential with center 'x0' and force constant 'k'". """
+    return 0.5 * k * (x - xopt)*(x - xopt)
+
+def potential_cosine(x, x0, k):
+    """Evaluate at 'x' a cosine function with center 'x0' and force constant 'k'.
+    Note: x has units of degrees."""
+    return k * (1. - np.cos((x - x0)*(np.pi/180.0)))
     
