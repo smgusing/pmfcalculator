fb59aef62c99c647f00a77754a544e8c7bd2ac20
Alex Chen <axc_elnino@hotmail.com>
2014-08-25 13:17:19 -0600

    First complete draft of a new version of potentials.py. Untested.

--- src/potentials.py
+++ src/potentials.py
@@ -2,39 +2,45 @@
 
 import numpy as np
 import itertools
-from operator import mul
 
 
-# Expose only this function:
-def biasingPotentials(?):
-    return _combinePotentials( _readFunctions(?)
-                             , _readArgLists(?)
-                             , _readEdgeLists(?)
-                             )
+# ----------------------------------------------------------------
+# Interface
+
+def biasPotential(functionNames, collvars, vardict, histogramEdgeLists):
+    return _biasPotential( _readFunctions(functionNames)
+                         , _readArgLists(collvars, vardict)
+                         , _readEdgeLists(histogramEdgeLists)
+                         )
             
-# Transform functions, argLists, edgeLists from whatever form in which they are available.
 def _readFunctions(functionNames):
     return [ lookupDict_potentials[fn] for fn in functionNames ]
 
-def _readArgLists(vardict, collvars):
+def _readArgLists(collvars, vardict):
     return [ zip(vardict[i+'_fc'],vardict[i + '_x0']) for i in collvars ]
 
-def _readEdgeLists(?):
-    pass
+def _readEdgeLists(edgeLists):
+    # identity function
+    return edgeLists 
 
-# I am not yet sure in what forms the arguments of this function are available.
-# At the moment, it takes a list of functions (fs[dim])
-#                         a list of lists of argument tuples (argLists[dim][i]), and
-#                         a list of lists of scalars (edgeLists[dim][i]),
-#     where indexing is shown by e.g. [dim] (reaction coordinate dimension index), or
-#                                     [i]   (unnamed index).
+# ----------------------------------------------------------------
+# Implementation
+
 # :: [(Arg -> Double -> Double)] -> [[Arg]] -> [[Double]] -> [[Double]]
-def _biasingPotentials(fs, argLists, edgeLists):
+def _biasPotential(fs, argLists, edgeLists):
+    """
+    Argument types:
+        fs:         a list of functions (fs[dim])
+        argLists:   a list of lists of argument tuples (argLists[dim][i]), and
+        edgeLists:  a list of lists of scalars (edgeLists[dim][i]),
+        where indexing is shown by e.g. [dim] (reaction coordinate dimension index), or
+                                        [i]   (unnamed index).
+    """
     simulationParameters = list(itertools.product(*argLists))
     simulationPotentials = map(lambda p: _combinePotentials(fs, p), simulationParameters)
     binCoordinates = list(itertools.product(*map(_binCenters, edgeLists)))
     return np.reshape((len(simulationPotentials), len(binCoordinates))
-                      np.array([ f(p) for f in simulationPotentials for p in binCoordinates ])
+                      , np.array([ f(p) for f in simulationPotentials for p in binCoordinates ])
                       )
 
 # :: [(Arg -> Double -> Double)] -> [Arg] -> (Vector Double -> Double)
@@ -44,7 +50,7 @@ def _combinePotentials(fs, argTuples):
         for i in xrange(len(xs)):
             acc += fs[i](xs[i], *argTuples[i])
         return acc
-    return combinedPotentials
+    return combinedPotential
 
 # :: [Double] -> [Double]
 def _binCenters(edgeList):
@@ -55,16 +61,16 @@ def _binCenters(edgeList):
 # Potential functions.
 # Note: potential functions must have call signatures of the type (Double, *args).
 
-lookupDict_potentials = { "harmonic"  : potential_harmonic
-                        , "cosine"    : potential_cosine
-                        }
-
 def potential_harmonic(x, x0, k):
     """Evaluate at 'x' a harmonic potential with center 'x0' and force constant 'k'". """
-    return 0.5 * k * (x - xopt)*(x - xopt)
+    return 0.5 * k * (x - x0)*(x - x0)
 
 def potential_cosine(x, x0, k):
     """Evaluate at 'x' a cosine function with center 'x0' and force constant 'k'.
     Note: x has units of degrees."""
     return k * (1. - np.cos((x - x0)*(np.pi/180.0)))
     
+lookupDict_potentials = { "harmonic"  : potential_harmonic
+                        , "cosine"    : potential_cosine
+                        }
+
